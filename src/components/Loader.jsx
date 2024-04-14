004258ca53bf08246db7b86dfbaa7fda9afe26c6
Mishyn-Kostiantyn <129659279+Mishyn-Kostiantyn@users.noreply.github.com>
2024-04-14 12:52:16 +0300

    Модуль 5. ДЗ

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import css from './Loader.module.css';
+
+
 import { ColorRing } from 'react-loader-spinner';
 const Loader = () => {
   return (
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-hot-toast';
+import MovieList from '../components/MovieList';
+import { requestTrendingMovies } from '../services/HTTPRequest/HTTPRequest';
+import Loader from '../components/Loader';
+
+const HomePage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [trendingMovies, setTrendingMovies] = useState(null);
+  
+   useEffect(() => {
+        async function fetchTrendingMovies() {
+      try {
+        setIsLoading(true);
+        const data = await requestTrendingMovies();
+
+        // console.log(data);
+        if (data.results && data.results.length > 0) {
+          setTrendingMovies(data.results) } else { toast('Sorry, there are no trending movies at the moment. Please try again with anover search term!'); }
+              }
+      catch (error) { setIsError(true); }
+      finally { setIsLoading(false); }
+    }
+    fetchTrendingMovies();
+  }, []);
+  return (
+    <div>
+      {isLoading && <Loader />}
+      {isError && toast('Sorry, there are some problems. Please try reloading the page!')}
+      <MovieList listOfMovies={trendingMovies} /></div>
+  )
+}
+
+export default HomePage
